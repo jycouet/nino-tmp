35b6477d66866a864db0feec68009bbb75a9f2fe
jycouet <jycouet@gmail.com>
2023-11-02 20:32:28 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { graphql } from "$houdini";
+
+  const carsList = graphql(`
+    query CarsList @load {
+      cars(limit: 1) {
+        status
+        title
+        condition
+      }
+    }
+  `);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+{JSON.stringify($carsList, null, 2)}
